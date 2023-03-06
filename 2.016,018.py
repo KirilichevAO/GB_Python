bbecfdc99a9c73064f924ae599881f8262ec76f3
KirilichevAO <kirilichev.a@icloud.com>
2023-03-06 16:04:57 +0700

    06.03.23

--- 2.016,018.py
+++ 2.016,018.py
@@ -3,3 +3,20 @@
 # Программа должна вывести в консоль сколько раз встречается в заданном списке искомое число X,
 # которое мы вводим с клавиатуры, либо выводим на экран, максимально близкое ему по значению
 
+import random
+
+my_list = [random.randint(0, 100) for _ in range(10)]
+number = int(input('Введите искомое число: '))
+list_negativ = []
+list_pozitiv = []
+print(my_list)
+
+if number not in my_list:
+    for i in my_list:
+        if number > i:
+            list_negativ.append(i)
+        elif number < i:
+            list_pozitiv.append(i)
+    print(max(list_negativ), min(list_pozitiv))
+else:
+    print(my_list.count(number))
