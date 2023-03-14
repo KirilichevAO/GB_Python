86cf3a13537df661baa425eafb56345c875decf7
North13 <kirilichev.a@icloud.com>
2023-03-14 11:19:13 +0700

    14.03.23

--- 2.030.py
+++ 2.030.py
@@ -4,3 +4,22 @@
 
 # Ввод: 7 2 5
 # Вывод: 7 9 11 13 15
+
+# one_element = int(input('Введите значение первого элемента: '))
+# difference = int(input('Введите разность: '))
+# total_elements = int(input('Введите количество элементов: '))
+# n_element = one_element + (total_elements - 1) * difference
+# my_list = []
+#
+# for i in range(total_elements):
+#     if one_element <= n_element:
+#         my_list.append(one_element)
+#         one_element += difference
+# print(my_list)
+
+one_element = int(input('Введите значение первого элемента: '))
+difference = int(input('Введите разность: '))
+total_elements = int(input('Введите количество элементов: '))
+
+for i in range(total_elements):
+    print(one_element + i * difference, end=' ')
--- 2.032.py
+++ 2.032.py
@@ -2,15 +2,23 @@
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
 
 # Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# 5
+# 15
 # Вывод: [1, 9, 13, 14, 19]
 
-# ример: 1 2 3 5 8 15 23 38
-# Получить: [(2, 4), (8, 64), (38, 1444)]
+# my_list = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# first_nomber = int(input('Введите значение начального диапазона: '))
+# sekond_number = int(input('Введите значение конечного диапазона: '))
+# my_list_index = []
+#
+# for i in my_list:
+#     if i >= first_nomber and i <= sekond_number:
+#         my_list_index.append(my_list.index(i))
+# print(my_list_index)
 
-my_list = [1, 2, 3, 5, 8, 15, 23, 38]
-my_list_temp = []
-
-for i in my_list:
-    if i % 2 == 0:
-        my_list_temp.append(i**2)
-print(my_list_temp)
+my_list = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+first_nomber = int(input('Введите значение начального диапазона: '))
+sekond_number = int(input('Введите значение конечного диапазона: '))
+for i in range(len(my_list)):
+    if first_nomber <= my_list[i] <= sekond_number:
+        print(i, end=' ')
