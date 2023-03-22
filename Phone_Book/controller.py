ac6d914de839aaa1d3e355ff959c5880672d97a2
North13 <kirilichev.a@icloud.com>
2023-03-22 14:28:03 +0700

    22.03.23

--- Phone_Book/controller.py
+++ Phone_Book/controller.py
@@ -3,18 +3,30 @@ import model
 
 def start():
     while True:
+        pb = model.get_phone_book()
         choice = view.main_menu()
         match choice:
             case 1:
                 model.open_file()
+                view.show_massage('Файл успешно открыт!')
             case 2:
-                pass
+                model.save_file()
+                view.show_massage('Файл успешно сохранен!')
             case 3:
-                pb = model.phone_book()
                 view.show_contacts(pb, 'Телефонная книга пуста или не открыта')
             case 4:
-                pass
+                contact = view.add_contact()
+                model.add_contact(contact)
             case 5:
-                pass
-            case _:
-                pass
+                if view.show_contacts(pb, 'Телефонная книга пуста или не открыта'):
+                    index = view.input_index('Введите номер изменяемого контакта')
+                    contact = view.change_contact(pb, index)
+                    model.change_contact(contact, index)
+                    view.show_massage(f'Контакт {model.get_phone_book()[index - 1].get("name")} успешно изменен!')
+            case 6:
+                search = view.input_search('Введите искомый элемент')
+                result = model.find_contact(search)
+                view.show_contacts(result, 'Контакты не найдены!')
+            case 7:
+                return
+
--- Phone_Book/view.py
+++ Phone_Book/view.py
@@ -1,23 +1,53 @@
-
+import text_filds
 
 
 def main_menu() -> int:
-    return int(input('''Главное меню:
-    1. Открыть файл
-    2. Сохранить файл
-    3. Показать контакты
-    4. Добавить контакт
-    5. Изменить контакт
-Выберите пункт меню: '''))
+    print(text_filds.main_menu)
+    lenght_menu = len(text_filds.main_menu.split('\n')) - 1
+    while True:
+        choice = input('Выберите пункт меню: ')
+        if choice.isdigit() and 0 < int(choice) <= lenght_menu:
+            return int(choice)
+        else:
+            print(f'Введите ЧИСЛО от 1 до {lenght_menu}')
 
 
 def show_contacts(book: list[dict], error_massage: str):
     if not book:
         print(error_massage)
+        return False
     else:
         for i, contact in enumerate(book):
             print(f'{i}. {contact.get("name"): <20} '
                   f'{contact.get("phone"): <20} '
                   f'{contact.get("comment"): <20}')
+        return True
 
 
+def add_contact() -> dict:
+     name = input('Введите имя: ')
+     phone = input('Введите номер телефона: ')
+     comment = input('Введите комментарий: ')
+     return {'name': name, 'phone': phone, 'comment': comment}
+
+
+def input_index(massege: str):
+    return int(input(massege))
+
+
+def input_search(massege):
+    return input(massege)
+
+
+def change_contact(book: list[dict], index: int):
+    print('Введите новые данные или оставьте пустое поле, если нет изменений')
+    contact = add_contact()
+    return {'name': contact.get('name') if contact.get('name') else book[index - 1].get('name'),
+            'phone': contact.get('phone') if contact.get('phone') else book[index - 1].get('phone'),
+            'comment': contact.get('comment') if contact.get('comment') else book[index - 1].get('comment')}
+
+
+def show_massage(message: str):
+    print('-' * len(message))
+    print(message)
+    print('-' * len(message))
